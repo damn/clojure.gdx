eceef451528b8818964350299d955ece6f768979
Michael Sappler <michaelsappler@gmail.com>
2024-12-12 19:43:42 +0100

    Add clojure.gdx.utils.disposable & clojure.gdx.math.utils

---
+++ src/clojure/gdx/math/utils.clj
@@ -0,0 +1,11 @@
+(ns clojure.gdx.math.utils
+  (:import (com.badlogic.gdx.math MathUtils)))
+
+(defn equal? [a b]
+  (MathUtils/isEqual a b))
+
+(defn clamp [value min max]
+  (MathUtils/clamp (float value) (float min) (float max)))
+
+(defn degree->radians [degree]
+  (* MathUtils/degreesToRadians (float degree)))
