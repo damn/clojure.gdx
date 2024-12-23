90e5f1b7b2f0c11d87fbf5502b6f245d0668a17d
Michael Sappler <michaelsappler@gmail.com>
2024-12-23 23:30:12 +0100

    Move internal function to gdx.interop

--- src/clojure/gdx.clj
+++ src/clojure/gdx.clj
@@ -1,20 +1,15 @@
 (ns clojure.gdx
   "Bindings for core LibGDX components, providing simplified access
-  to application, audio, graphics, input, and networking systems."
-  (:import (com.badlogic.gdx Gdx)))
+  to application, audio, graphics, input, and networking systems.")
 
-(defn bind-gdx-components
-  "Creates global bindings for core LibGDX components in the current namespace,
-  simplifying access to application, audio, graphics, input, and networking systems."
-  []
-  (def app      Gdx/app)
-  (def audio    Gdx/audio)
-  (def files    Gdx/files)
-  (def gl       Gdx/gl)
-  (def gl20     Gdx/gl20)
-  (def gl30     Gdx/gl30)
-  (def gl31     Gdx/gl31)
-  (def gl32     Gdx/gl32)
-  (def graphics Gdx/graphics)
-  (def input    Gdx/input)
-  (def net      Gdx/net))
+(declare app
+         audio
+         files
+         gl
+         gl20
+         gl30
+         gl31
+         gl32
+         graphics
+         input
+         net)
--- src/clojure/gdx/interop.clj
+++ src/clojure/gdx/interop.clj
@@ -1,6 +1,26 @@
 (ns clojure.gdx.interop
-  (:require [clojure.string :as str])
-  (:import (com.badlogic.gdx.utils Align)))
+  (:require [clojure.gdx :as gdx]
+            [clojure.string :as str])
+  (:import (com.badlogic.gdx Gdx)
+           (com.badlogic.gdx.utils Align)))
+
+(defn bind-clojure.gdx-components
+  "Creates global bindings for core LibGDX components in the `clojure.gdx` namespace,
+  simplifying access to application, audio, graphics, input, and networking systems.
+
+  Call this in `application/create` in a libgdx application backend implementation."
+  []
+  (.bindRoot #'gdx/app      Gdx/app)
+  (.bindRoot #'gdx/audio    Gdx/audio)
+  (.bindRoot #'gdx/files    Gdx/files)
+  (.bindRoot #'gdx/gl       Gdx/gl)
+  (.bindRoot #'gdx/gl20     Gdx/gl20)
+  (.bindRoot #'gdx/gl30     Gdx/gl30)
+  (.bindRoot #'gdx/gl31     Gdx/gl31)
+  (.bindRoot #'gdx/gl32     Gdx/gl32)
+  (.bindRoot #'gdx/graphics Gdx/graphics)
+  (.bindRoot #'gdx/input    Gdx/input)
+  (.bindRoot #'gdx/net      Gdx/net))
 
 (defn static-field [klass-str k]
   (eval (symbol (str "com.badlogic.gdx." klass-str "/" (str/replace (str/upper-case (name k)) "-" "_")))))
