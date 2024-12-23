bcf770bcdebb64a32e66bf07ecf2bbc19bd2e2aa
Michael Sappler <michaelsappler@gmail.com>
2024-12-23 19:36:10 +0100

    Add comments to clojure.gdx.input

--- src/clojure/gdx/input.clj
+++ src/clojure/gdx/input.clj
@@ -1,14 +1,27 @@
 (ns clojure.gdx.input
+  "Interface to the input facilities. This allows polling the state of the keyboard, the touch screen and the accelerometer. On some backends (desktop, gwt, etc) the touch screen is replaced by mouse input. The accelerometer is of course not available on all backends.
+
+  Instead of polling for events, one can process all input events with an InputProcessor. You can set the InputProcessor via the setInputProcessor(InputProcessor) method. It will be called before the ApplicationListener.render() method in each frame.
+
+  Keyboard keys are translated to the constants in Input.Keys transparently on all systems. Do not use system specific key constants.
+
+  The class also offers methods to use (and test for the presence of) other input systems like vibration, compass, on-screen keyboards, and cursor capture. Support for simple input dialogs is also provided. "
   (:require [clojure.gdx.interop :refer [k->input-button k->input-key]])
   (:import (com.badlogic.gdx Gdx)))
 
-(defn x []
+(defn x
+  "The x coordinate of the last touch on touch screen devices and the current mouse position on desktop for the first pointer in screen coordinates. The screen origin is the top left corner."
+  []
   (.getX Gdx/input))
 
-(defn y []
+(defn y
+  "The y coordinate of the last touch on touch screen devices and the current mouse position on desktop for the first pointer in screen coordinates. The screen origin is the top left corner."
+  []
   (.getY Gdx/input))
 
-(defn button-just-pressed? [b]
+(defn button-just-pressed?
+  "Returns whether a given button has just been pressed. Button constants can be found in Input.Buttons. On Android only the Buttons#LEFT constant is meaningful before version 4.0. On WebGL (GWT), only LEFT, RIGHT and MIDDLE buttons are supported."
+  [b]
   (.isButtonJustPressed Gdx/input (k->input-button b)))
 
 (defn key-just-pressed? [k]
