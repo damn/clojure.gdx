8f48e655be7bd1eb38cb78c2920e0ffc06d0a4d2
Michael Sappler <michaelsappler@gmail.com>
2024-12-23 20:07:38 +0100

    Add comments

--- src/clojure/gdx/assets.clj
+++ src/clojure/gdx/assets.clj
@@ -1,8 +1,11 @@
 (ns clojure.gdx.assets
+  "Loads and stores assets like textures, bitmapfonts, tile maps, sounds, music and so on."
   (:refer-clojure :exclude [load type])
   (:import (com.badlogic.gdx.assets AssetManager)))
 
-(defn manager ^AssetManager []
+(defn manager
+  "Creates a new AssetManager with all default loaders."
+  ^AssetManager []
   (proxy [AssetManager clojure.lang.IFn] []
     (invoke [^String path]
       (if (AssetManager/.contains this path)
@@ -14,17 +17,28 @@
     :sound   com.badlogic.gdx.audio.Sound
     :texture com.badlogic.gdx.graphics.Texture))
 
-(defn load [manager file asset-type]
+(defn load
+  "Adds the given asset to the loading queue of the AssetManager."
+  [manager file asset-type]
   (AssetManager/.load manager ^String file (asset-type->class asset-type)))
 
-(defn finish-loading [manager]
+(defn finish-loading
+  "Blocks until all assets are loaded."
+  [manager]
   (AssetManager/.finishLoading manager))
 
-(defn type [manager asset-name]
+(defn type
+  "Returns:
+  the type of a loaded asset. "
+  [manager asset-name]
   (asset-type->class (AssetManager/.getAssetType manager asset-name)))
 
-(defn names [manager]
+(defn names
+  "the file names of all loaded assets."
+  [manager]
   (AssetManager/.getAssetNames manager))
 
-(defn dispose [manager]
+(defn dispose
+  "Disposes all assets in the manager and stops all asynchronous loading."
+  [manager]
   (AssetManager/.dispose manager))
