52fc361837f735b24409255d183228a919978c22
Michael Sappler <michaelsappler@gmail.com>
2024-12-26 18:43:30 +0100

    Add docstrings

--- src/clojure/gdx.clj
+++ src/clojure/gdx.clj
@@ -11,7 +11,9 @@
            (com.badlogic.gdx.utils Disposable ScreenUtils)
            (com.badlogic.gdx.utils.viewport FitViewport Viewport)))
 
-(defn context []
+(defn context
+  "Returns a map of the libgdx context. Call after application `create`."
+  []
   {:app      Gdx/app
    :audio    Gdx/audio
    :files    Gdx/files
@@ -24,43 +26,106 @@
    :input    Gdx/input
    :net      Gdx/net})
 
-(defn exit [context]
+(defn exit
+  "Schedule an exit from the application. On android, this will cause a call to `pause` and `dispose` some time in the future,
+  it will not immediately finish your application. On iOS this should be avoided in production as it breaks Apples guidelines."
+  [context]
   (Application/.exit (:app context)))
 
-(defn post-runnable [context runnable]
+(defn post-runnable
+  "Posts a Runnable on the main loop thread. In a multi-window application, the [context] values may be unpredictable at the time the Runnable is executed. If graphics or input are needed, they should be bound to a variable to be used in the Runnable."
+  [context runnable]
   (Application/.postRunnable (:app context) runnable))
 
-(defn internal-file [context file]
+(defn internal-file
+  "Path relative to the asset directory on Android and to the application's root directory on the desktop. On the desktop, if the file is not found, then the classpath is checked. This enables files to be found when using JWS or applets. Internal files are always readonly."
+  [context file]
   (Files/.internal (:files context) file))
 
-(defn delta-time [context]
+(defn delta-time
+  "The time span between the current frame and the last frame in seconds."
+  [context]
   (Graphics/.getDeltaTime (:graphics context)))
 
-(defn frames-per-second [context]
+(defn frames-per-second
+  "The average number of frames per second."
+  [context]
   (Graphics/.getFramesPerSecond (:graphics context)))
 
-(defn cursor [context pixmap hotspot-x hotspot-y]
+(defn cursor
+  "Create a new cursor represented by the Pixmap. The Pixmap must be in RGBA8888 format, width & height must be powers-of-two greater than zero (not necessarily equal) and of a certain minimum size (32x32 is a safe bet), and alpha transparency must be single-bit (i.e., 0x00 or 0xFF only). This function returns a Cursor object that can be set as the system cursor by calling setCursor(Cursor) .
+
+  Parameters:
+
+  pixmap - the mouse cursor image as a Pixmap
+
+  xHotspot - the x location of the hotspot pixel within the cursor image (origin top-left corner)
+
+  yHotspot - the y location of the hotspot pixel within the cursor image (origin top-left corner)
+
+  Returns:
+
+  a cursor object that can be used by calling setCursor(Cursor) or null if not supported"
+  [context pixmap hotspot-x hotspot-y]
   (Graphics/.newCursor (:graphics context) pixmap hotspot-x hotspot-y))
 
-(defn set-cursor [context cursor]
+(defn set-cursor
+  "Only viable on the lwjgl-backend and on the gwt-backend. Browsers that support cursor:url() and support the png format (the pixmap is converted to a data-url of type image/png) should also support custom cursors. Will set the mouse cursor image to the image represented by the Cursor. It is recommended to call this function in the main render thread, and maximum one time per frame.
+
+  Parameters:
+  cursor - the mouse cursor as a Cursor"
+  [context cursor]
   (Graphics/.setCursor (:graphics context) cursor))
 
-(defn input-x [context]
+(defn input-x
+  "The x coordinate of the last touch on touch screen devices and the current mouse position on desktop for the first pointer in screen coordinates. The screen origin is the top left corner."
+  [context]
   (Input/.getX (:input context)))
 
-(defn input-y [context]
+(defn input-y
+  "The y coordinate of the last touch on touch screen devices and the current mouse position on desktop for the first pointer in screen coordinates. The screen origin is the top left corner."
+  [context]
   (Input/.getY (:input context)))
 
-(defn button-just-pressed? [context b]
+(defn button-just-pressed?
+  "Returns whether a given button has just been pressed. Button constants can be found in Input.Buttons. On Android only the Buttons#LEFT constant is meaningful before version 4.0.
+
+  Parameters:
+  button - the button to check.
+
+  Returns:
+  true or false."
+  [context b]
   (Input/.isButtonJustPressed (:input context) (k->input-button b)))
 
-(defn key-just-pressed? [context k]
+(defn key-just-pressed?
+  "Returns whether the key has just been pressed.
+
+  Parameters:
+  key - The key code as found in Input.Keys.
+
+  Returns:
+  true or false. "
+  [context k]
   (Input/.isKeyJustPressed (:input context) (k->input-key k)))
 
-(defn key-pressed? [context k]
+(defn key-pressed?
+  "Returns whether the key is pressed.
+
+  Parameters:
+  key - The key code as found in Input.Keys.
+
+  Returns:
+  true or false."
+  [context k]
   (Input/.isKeyPressed (:input context) (k->input-key k)))
 
-(defn set-input-processor [context input-processor]
+(defn set-input-processor
+  "Sets the InputProcessor that will receive all touch and key input events. It will be called before the `ApplicationListener.render()` method each frame.
+
+  Parameters:
+  processor - the InputProcessor "
+  [context input-processor]
   (Input/.setInputProcessor (:input context) input-processor))
 
 (defn play
@@ -72,16 +137,25 @@
   (Sound/.play sound))
 
 (defn sprite-batch
-  "Constructs a new `com.badlogic.gdx.graphics.g2d.SpriteBatch` with a size of 1000, one buffer, and the default shader."
+  "Draws batched quads using indices.
+
+  Constructs a new sprite-batch with a size of 1000, one buffer, and the default shader."
   []
   (SpriteBatch.))
 
-(def dispose Disposable/.dispose)
+(defn dispose
+  "Releases all resources of this object."
+  [disposable]
+  (Disposable/.dispose disposable))
 
-(defn orthographic-camera []
+(defn orthographic-camera
+  "A camera with orthographic projection."
+  []
   (OrthographicCamera.))
 
-(defn equal? [a b]
+(defn equal?
+  "Returns true if a is nearly equal to b. The function uses the default floating error tolerance."
+  [a b]
   (MathUtils/isEqual a b))
 
 (defn clamp [value min max]
@@ -90,10 +164,11 @@
 (defn degree->radians [degree]
   (* MathUtils/degreesToRadians (float degree)))
 
-(def ^Color black Color/BLACK)
-(def ^Color white Color/WHITE)
+(def ^Color ^{:doc "The color black."} black Color/BLACK)
+(def ^Color ^{:doc "The color white."} white Color/WHITE)
 
 (defn color
+  "Creates a color object, holding the r, g, b and alpha component as floats in the range [0,1]. All methods perform clamping on the internal values after execution."
   ([r g b]
    (color r g b 1))
   ([r g b a]
@@ -148,18 +223,29 @@
   (Colors/put name-str color))
 
 (defn pixmap
+  "A Pixmap represents an image in memory. It has a width and height expressed in pixels as well as a Pixmap.Format specifying the number and order of color components per pixel. Coordinates of pixels are specified with respect to the top left corner of the image, with the x-axis pointing to the right and the y-axis pointing downwards.
+
+  By default all methods use blending. You can disable blending with setBlending(Blending), which may reduce blitting time by ~30%. The drawPixmap(Pixmap, int, int, int, int, int, int, int, int) method will scale and stretch the source image to a target image. There either nearest neighbour or bilinear filtering can be used.
+
+  A Pixmap stores its data in native heap memory. It is mandatory to call dispose() when the pixmap is no longer needed, otherwise memory leaks will result"
   ([^FileHandle file-handle]
    (Pixmap. file-handle))
   ([width height ^Pixmap$Format format]
    (Pixmap. (int width) (int height) format)))
 
-(defn draw-pixel [^Pixmap pixmap x y]
+(defn draw-pixel
+  "Draws a pixel at the given location with the current color."
+  [^Pixmap pixmap x y]
   (.drawPixel pixmap x y))
 
-(defn clear-screen [color]
+(defn clear-screen
+  "Clears the color currently bound OpenGL frame buffer with the specified Color."
+  [color]
   (ScreenUtils/clear color))
 
-(defn fit-viewport [width height camera]
+(defn fit-viewport
+  "A ScalingViewport that uses Scaling.fit so it keeps the aspect ratio by scaling the world up to fit the screen, adding black bars (letterboxing) for the remaining space."
+  [width height camera]
   (proxy [FitViewport ILookup] [width height camera]
     (valAt
       ([key]
@@ -186,7 +272,10 @@
   [viewport w h & {:keys [center-camera?]}]
   (Viewport/.update viewport w h (boolean center-camera?)))
 
-(defmulti overlaps? (fn [a b] [(class a) (class b)]))
+(defmulti
+  ^{:doc "Checks if the two shapes (circle or rectangle) are overlapping."}
+  overlaps?
+  (fn [a b] [(class a) (class b)]))
 
 (defmethod overlaps? [Circle Circle]
   [^Circle a ^Circle b]
@@ -205,6 +294,11 @@
   (Intersector/overlaps circle rect))
 
 (defn texture-region
+  "Defines a rectangular area of a texture. The coordinate system used has its origin in the upper left corner with the x-axis pointing to the right and the y axis pointing downwards.
+
+  Parameters:
+  width - The width of the texture region. May be negative to flip the sprite when drawn.
+  height - The height of the texture region. May be negative to flip the sprite when drawn."
   ([^Texture texture]
    (TextureRegion. texture))
   ([^Texture texture x y w h]
@@ -219,7 +313,9 @@
   [^TextureRegion texture-region x y w h]
   (TextureRegion. texture-region (int x) (int y) (int w) (int h)))
 
-(defn dimensions [^TextureRegion texture-region]
+(defn dimensions
+  "Returns the region's width and height as vector [w h]."
+  [^TextureRegion texture-region]
   [(.getRegionWidth  texture-region)
    (.getRegionHeight texture-region)])
 
@@ -236,7 +332,9 @@
   [^Pixmap pixmap]
   (Texture. pixmap))
 
-(defn asset-manager []
+(defn asset-manager
+  "Loads and stores assets like textures, bitmapfonts, tile maps, sounds, music and so on."
+  []
   (proxy [AssetManager IFn] []
     (invoke [^String path]
       (if (AssetManager/.contains this path)
