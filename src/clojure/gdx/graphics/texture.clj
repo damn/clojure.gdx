bfe5c3596f86808d629c1d2118db6b8b239068e1
Michael Sappler <michaelsappler@gmail.com>
2024-12-26 15:12:18 +0100

    Add namespace doc to texture

--- src/clojure/gdx/graphics/texture.clj
+++ src/clojure/gdx/graphics/texture.clj
@@ -1,7 +1,18 @@
 (ns clojure.gdx.graphics.texture
+  "A Texture wraps a standard OpenGL ES texture.
+
+  A Texture can be managed. If the OpenGL context is lost all managed textures get invalidated. This happens when a user switches to another application or receives an incoming call. Managed textures get reloaded automatically.
+
+  A Texture has to be bound via the `GLTexture.bind()` method in order for it to be applied to geometry. The texture will be bound to the currently active texture unit specified via `GL20.glActiveTexture(int)`.
+
+  You can draw Pixmaps to a texture at any time. The changes will be automatically uploaded to texture memory. This is of course not extremely fast so use it with care. It also only works with unmanaged textures.
+
+  A Texture must be disposed when it is no longer used"
   (:import (com.badlogic.gdx.graphics Texture
                                       Texture$TextureFilter
                                       Pixmap)))
+
+; TODO file-handle ...
 
 (defn create [^Pixmap pixmap]
   (Texture. pixmap))
