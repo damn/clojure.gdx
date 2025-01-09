db535df341952a3287ebe52a27fdda0257006c2e
Michael Sappler <michaelsappler@gmail.com>
2025-01-09 11:46:38 +0100

    Finish Files API

--- src/clojure/files.clj
+++ src/clojure/files.clj
@@ -1,4 +1,13 @@
 (ns clojure.files)
 
 (defprotocol Files
-  (internal [_ path] "Convenience method that returns a {@link FileType#Internal} file handle."))
+  (internal [_ path])
+  (classpath [_ path])
+  (external [_ path])
+  (absolute [_ path])
+  (local [_ path])
+  (external-storage-path [_])
+  (external-storage-available? [_])
+  (local-storage-path [_])
+  (local-storage-available? [_])
+  (file-handle [_ path file-type]))
