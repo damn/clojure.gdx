f43e1267c0efc9e3873dbb84f775d3f31103e4c8
Michael Sappler <michaelsappler@gmail.com>
2025-09-06 16:26:11 +0530

    add stage ctx and create

--- src/clojure/gdx/scenes/scene2d/stage.clj
+++ src/clojure/gdx/scenes/scene2d/stage.clj
@@ -1,5 +1,7 @@
 (ns clojure.gdx.scenes.scene2d.stage
-  (:import (com.badlogic.gdx.scenes.scene2d Stage)))
+  (:require [clojure.gdx.scenes.scene2d.group :as group])
+  (:import (clojure.lang ILookup)
+           (clojure.gdx.scenes.scene2d Stage)))
 
 (defn act! [stage]
   (Stage/.act stage))
@@ -18,3 +20,14 @@
 
 (defn hit [stage [x y]]
   (Stage/.hit stage x y true))
+
+(defn get-ctx [^Stage stage]
+  @(.ctx ^Stage stage))
+
+(defn set-ctx! [^Stage stage ctx]
+  (reset! (.ctx ^Stage stage) ctx))
+
+(defn create [viewport batch]
+  (proxy [Stage ILookup] [viewport batch (atom nil)]
+    (valAt [id]
+      (group/find-actor-with-id (root this) id))))
