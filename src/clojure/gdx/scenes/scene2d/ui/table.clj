3933eccf2d8284ecd99f35ed5a3b4fe11ae68019
Michael Sappler <michaelsappler@gmail.com>
2025-09-06 19:38:18 +0530

    add table stuff

--- src/clojure/gdx/scenes/scene2d/ui/table.clj
+++ src/clojure/gdx/scenes/scene2d/ui/table.clj
@@ -1,15 +1,30 @@
 (ns clojure.gdx.scenes.scene2d.ui.table
-  (:import (com.badlogic.gdx.scenes.scene2d Actor)
-           (com.badlogic.gdx.scenes.scene2d.ui Table)))
+  (:require [clojure.gdx.scenes.scene2d.actor :as actor]
+            [clojure.gdx.scenes.scene2d.ui.cell :as cell]
+            [clojure.gdx.scenes.scene2d.ui.widget-group :as widget-group])
+  (:import (com.badlogic.gdx.scenes.scene2d.ui Table)))
 
-(defn add! [table ^Actor actor]
-  (Table/.add table actor))
+(defn add! [table actor-or-decl]
+  (Table/.add table (actor/build? actor-or-decl)))
 
-(defn cells [^Table table]
-  (.getCells table))
+(def cells Table/.getCells)
 
-(defn row! [table]
-  (Table/.row table))
+(defn add-rows!
+  "rows is a seq of seqs of columns.
+  Elements are actors or nil (for just adding empty cells ) or a map of
+  {:actor :expand? :bottom?  :colspan int :pad :pad-bottom}. Only :actor is required."
+  [table rows]
+  (doseq [row rows]
+    (doseq [props-or-actor row]
+      (cond
+       (map? props-or-actor) (-> (add! table (:actor props-or-actor))
+                                 (cell/set-opts! (dissoc props-or-actor :actor)))
+       :else (add! table props-or-actor)))
+    (Table/.row table))
+  table)
 
-(defn defaults [table]
-  (Table/.defaults table))
+(defn set-opts! [table {:keys [rows cell-defaults] :as opts}]
+  (cell/set-opts! (Table/.defaults table) cell-defaults)
+  (doto table
+    (add-rows! rows)
+    (widget-group/set-opts! opts)))
