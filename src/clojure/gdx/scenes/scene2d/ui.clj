acd69ae2cf60af0ba8b8440f76a98afc0d946d8e
Michael Sappler <michaelsappler@gmail.com>
2025-09-06 18:24:21 +0530

    build horizontal group

--- src/clojure/gdx/scenes/scene2d/ui.clj
+++ src/clojure/gdx/scenes/scene2d/ui.clj
@@ -1,14 +1,18 @@
 (ns clojure.gdx.scenes.scene2d.ui
   (:require [clojure.gdx.scenes.scene2d :as scene2d]
+            [clojure.gdx.scenes.scene2d :as actor]
             [clojure.gdx.scenes.scene2d.group :as group])
   (:import (com.badlogic.gdx.scenes.scene2d.ui HorizontalGroup
                                                Stack)))
 
-(defn horizontal-group [space pad]
+(defn horizontal-group [{:keys [space pad] :as opts}]
   (let [group (scene2d/proxy-group HorizontalGroup [])]
     (when space (.space group (float space)))
     (when pad   (.pad   group (float pad)))
-    group))
+    (group/set-opts! opts)))
 
 (defn stack []
   (scene2d/proxy-group Stack []))
+
+(defmethod actor/build :actor.type/horizontal-group [opts]
+  (horizontal-group opts))
