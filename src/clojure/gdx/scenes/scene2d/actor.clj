34406fd7eb2df3a8663c06eb3dcf28a29cf2ac95
Michael Sappler <michaelsappler@gmail.com>
2025-09-06 15:56:18 +0530

    Revert "add actor/construct"
    This reverts commit c21c984715e100fd3e91bb636dc99fd96c4c705a.

--- src/clojure/gdx/scenes/scene2d/actor.clj
+++ src/clojure/gdx/scenes/scene2d/actor.clj
@@ -3,22 +3,6 @@
                                             Touchable)
            (com.badlogic.gdx.math Vector2)))
 
-(defmulti create :actor/type)
-
-(defn construct? ^Actor [actor-declaration]
-  (try
-   (cond
-    (instance? Actor actor-declaration) actor-declaration
-    (map? actor-declaration) (create actor-declaration)
-    (nil? actor-declaration) nil
-    :else (throw (ex-info "Cannot find constructor"
-                          {:instance-actor? (instance? Actor actor-declaration)
-                           :map? (map? actor-declaration)})))
-   (catch Throwable t
-     (throw (ex-info "Cannot create-actor"
-                     {:actor-declaration actor-declaration}
-                     t)))))
-
 (defn create [act draw]
   (proxy [Actor] []
     (act [delta]
