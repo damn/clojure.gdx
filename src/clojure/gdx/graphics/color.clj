531d06d2d7e9be3b62d1ad83dd2a1eee0e5c9f9b
Michael Sappler <michaelsappler@gmail.com>
2025-10-07 14:13:03 +0530

    add named colors

--- src/clojure/gdx/graphics/color.clj
+++ src/clojure/gdx/graphics/color.clj
@@ -13,3 +13,8 @@
                      (float g)
                      (float b)
                      (float a)))
+
+(def black [0 0 0 1])
+(def white [1 1 1 1])
+(def gray  [0.5 0.5 0.5 1])
+(def red   [1 0 0 1])
